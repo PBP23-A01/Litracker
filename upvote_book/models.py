871780b6a767a0bc8f43a16a8376c25e11c8b279
muhammadfawwazarshads <muhammad.fawwaz22@ui.ac.id>
2023-10-26 20:00:28 +0700

    upvote_book models

--- upvote_book/models.py
+++ upvote_book/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
+from authentication.models import UserProfile
+from book.models import Book
 
-# Create your models here.
+
+class Upvote(models.Model):
+    user = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
+    upvote_count = models.PositiveIntegerField(default=0)
+
+    class Meta:
+        unique_together = ('user', 'book')
