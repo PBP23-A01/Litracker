c0c8cfe0d700c67330b027c75b08a89e444ed3c3
Henrysoed <henry.soedibjo@gmail.com>
2023-10-27 13:37:32 +0700

    test conflict

--- reading_history/models.py
+++ reading_history/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+# Create your models here.
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    # image = models.ImageField(default="default.png", null=True, blank=True)
+    is_admin = models.BooleanField(default=False)
+    is_user = models.BooleanField(default=False)
