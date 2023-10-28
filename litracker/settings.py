83e79fc85a5333fcc07800c580246272733abc41
muhammadfawwazarshads <muhammad.fawwaz22@ui.ac.id>
2023-10-28 20:03:26 +0700

    edit settings.py LOGIN_URL (kalau belum login tapi akses home akan redirect ke guest page)

--- litracker/settings.py
+++ litracker/settings.py
@@ -129,3 +129,7 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+LOGIN_URL = 'authentication:index' 
+# kalau belum login tapi akses home akan redirect ke guest page
