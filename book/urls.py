91122f3c6be770d30797ad121be884f60607c62b
daffagrahito <muhammad.daffa23@ui.ac.id>
2023-10-28 21:21:04 +0700

    Implementasi modals Homepage dan test views buat search

--- book/urls.py
+++ book/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from book.views import get_books, show_homepage
+from book.views import get_books, show_homepage, find_book
 
 app_name = 'book'
 
 urlpatterns = [
     path('api/book/', get_books, name='get_books'),
-    path('', show_homepage, name='show_homepage')
+    path('', show_homepage, name='show_homepage'),
+    path('find-book/', find_book, name='find_book'),
 ]
--- book/views.py
+++ book/views.py
@@ -1,8 +1,11 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+import json
+from django.shortcuts import render, redirect
+from django.http import HttpResponse, JsonResponse
 from django.core import serializers
 from book.models import Book
-from django.contrib.auth.decorators import login_required
+from book.forms import BookForm
+from django.contrib.auth.decorators import login_required, user_passes_test
+from django.views.decorators.csrf import csrf_exempt
 
 # Create your views here.
 def get_books(request):
@@ -10,9 +13,51 @@ def get_books(request):
     return HttpResponse(serializers.serialize("json", data), 
                         content_type="application/json")
 
-@login_required
 def show_homepage(request):
-    user = request.user
     books = Book.objects.all()
     context = {'books': books}
     return render(request, 'homepage.html', context)
+
+def is_admin(user):
+    return user.userprofile.is_admin
+
+@login_required
+@user_passes_test(is_admin)
+def tambah_buku(request):
+    if request.method == 'POST':
+        form = BookForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('homepage')
+    else:
+        form = BookForm()
+    return render(request, 'tambah_buku.html', {'form': form})
+
+@csrf_exempt
+def find_book(request):
+    if request.method == 'POST':
+        data = json.loads(request.body.decode('utf-8'))
+
+        title = data.get("title", "")
+        author = data.get("author", "")
+        publisher = data.get("publisher", "")
+        published_year = data.get("published_year", "")
+
+        # Construct a filter based on the provided parameters
+        filters = {}
+        
+        if title:
+            filters['title__icontains'] = title
+        if author:
+            filters['author__icontains'] = author
+        if publisher:
+            filters['publisher__icontains'] = publisher
+        if published_year:
+            filters['published_year__icontains'] = published_year
+
+        # Apply the filters to the Book queryset
+        books = Book.objects.filter(**filters).values()
+
+        return JsonResponse({'books': list(books)}, safe=False)
+    else:
+        return JsonResponse({'error': 'Method not allowed'}, status=405)
