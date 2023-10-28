7816dc4c4f57c6dd54c82a99a423bfa46990784d
AnindyaMaulida <anindya.dea1705@gmail.com>
2023-10-28 12:26:40 +0700

    nambah page add review

--- review_book/views.py
+++ review_book/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from book.models import Book
 from django.core import serializers
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseNotFound
 
 # Create your views here.
 def show_review(request):
@@ -13,31 +13,23 @@ def show_review(request):
 
 def get_books(request):
     data = Book.objects.all()
-    return HttpResponse(serializers.serialize("json", data), 
-                        content_type="application/json")
+    return HttpResponse(serializers.serialize("json", data), content_type="application/json")
+
+def add_review(request):
+    return render(request, 'add_review.html')
 
 
+def add_review_ajax(request):
+    if request.method == 'POST':
+        ulasan = request.POST.get("ulasan")
+        user = request.user
 
-# from django.shortcuts import render, redirect
-# from .models import ReviewBook
-# from .forms import ReviewForm  # Anda perlu membuat form untuk ulasan, saya akan menambahkan ini sebagai contoh
+        new_review_book = Book(ulasan=ulasan, user=user)
+        new_review_book.save()
 
-# def review_book_list(request):
-#     reviews = ReviewBook.objects.all()
-#     return render(request, 'review_book/review_book_list.html', {'reviews': reviews})
+        return HttpResponse(b"CREATED", status=201)
 
-# def add_review(request, book_id):
-#     if request.method == 'POST':
-#         form = ReviewForm(request.POST)
-#         if form.is_valid():
-#             text = form.cleaned_data['text']
-#             user = request.user.userprofile  # Menggunakan UserProfile yang sudah login
-#             book = Book.objects.get(id=book_id)
+    return HttpResponseNotFound()
+    
 
-#             ReviewBook.objects.create(user=user, book=book, text=text)
-#             return redirect('review_book_list')
 
-#     else:
-#         form = ReviewForm()
-
-#     return render(request, 'review_book/add_review.html', {'form': form})
