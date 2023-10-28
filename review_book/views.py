d0df18f2aad94414710d491a8ad45589f6389731
AnindyaMaulida <anindya.dea1705@gmail.com>
2023-10-28 12:18:03 +0700

    benerin button  add review sm back

--- review_book/views.py
+++ review_book/views.py
@@ -34,28 +34,3 @@ def add_review_ajax(request):
     
 
 
-
-# from django.shortcuts import render, redirect
-# from .models import ReviewBook
-# from .forms import ReviewForm  # Anda perlu membuat form untuk ulasan, saya akan menambahkan ini sebagai contoh
-
-# def review_book_list(request):
-#     reviews = ReviewBook.objects.all()
-#     return render(request, 'review_book/review_book_list.html', {'reviews': reviews})
-
-# def add_review(request, book_id):
-#     if request.method == 'POST':
-#         form = ReviewForm(request.POST)
-#         if form.is_valid():
-#             text = form.cleaned_data['text']
-#             user = request.user.userprofile  # Menggunakan UserProfile yang sudah login
-#             book = Book.objects.get(id=book_id)
-
-#             ReviewBook.objects.create(user=user, book=book, text=text)
-#             return redirect('review_book_list')
-
-#     else:
-#         form = ReviewForm()
-
-#     return render(request, 'review_book/add_review.html', {'form': form})
-
