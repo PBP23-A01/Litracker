c6360b9b9c39e95be922c157ef59ca6f4884feaf
daffagrahito <muhammad.daffa23@ui.ac.id>
2023-10-29 18:29:17 +0700

    Fitur search sinkronus

--- book/urls.py
+++ book/urls.py
@@ -6,7 +6,7 @@ app_name = 'book'
 urlpatterns = [
     path('api/book/', get_books, name='get_books'),
     path('', show_homepage, name='show_homepage'),
-    path('search-books/', search_books, name='search_books'),
+    path('book/search-books/', search_books, name='search_books'),
     path('upvote_book/<int:book_id>/', upvote_book, name='upvote_book'),
     path('wishlist_book/<int:book_id>/', wishlist_book, name='wishlist_book'),
 ]
