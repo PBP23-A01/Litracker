d8d2c1a7a4999a9c32ac1745d28fbaf40fe5697f
muhammadfawwazarshadsaid <muhammad.fawwaz22@ui.ac.id>
2023-12-19 17:35:38 +0700

    lupa apus heheh

--- upvote_book/urls.py
+++ upvote_book/urls.py
@@ -1,12 +1,11 @@
 from django.urls import path
-from upvote_book.views import get_upvoted_books, upvote_book, toggle_upvote_flutter, get_upvoting_users, get_top_books, unvote_flutter
+from upvote_book.views import get_upvoted_books, upvote_book, toggle_upvote_flutter, get_upvoting_users, get_top_books
 
 app_name = 'upvote_book'
 
 urlpatterns = [
     path('', upvote_book, name='upvote_book'),
-    path('toggle_upvote_flutter/<int:book_id>/', toggle_upvote_flutter, name='toggle_upvote_flutter'),    
-    path('unvote_flutter/<int:book_id>/', unvote_flutter, name='unvote_flutter'),   
+    path('toggle_upvote_flutter/<int:book_id>/', toggle_upvote_flutter, name='toggle_upvote_flutter'),  
     path('get_upvoting_users/<int:book_id>/', get_upvoting_users, name='get_upvoting_users'),
     path('get_upvoted_books/', get_upvoted_books, name='get_upvoted_books'),
     path('get_top_books/', get_top_books, name='get_top_books'),
